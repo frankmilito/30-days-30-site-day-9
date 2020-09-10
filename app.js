a7198f509fc30e81e73e96b1b23e0220a3fbdbdf
frankmilito <frankobidike20@gmail.com>
2020-09-10 01:08:07 -0500

    Day 9 challenge

---
+++ app.js
@@ -0,0 +1,22 @@
+//   const countUp = new CountUp('number', 10000);
+//         if (!countUp.error) {
+//             countUp.start();
+//         } else {
+//             console.error(countUp.error);
+//         }
+//         countUp.start()
+var menu = document.getElementById("menu");
+var nav = document.getElementById("nav");
+var exit = document.getElementById("exit");
+
+menu.addEventListener("click", function (e) {
+    // nav.classList.toggle("menu-items");
+    // nav.style.transform = "scaleY(1)";
+    nav.style.width= '100%';
+    e.preventDefault();
+});
+exit.addEventListener("click", function (e) {
+    // nav.classList.add("menu-items");
+    nav.style.width = "0";
+    e.preventDefault();
+});
